c57642b323a5ac2417237e7c511b5c1d82bfe6ff
Shainkey Aditya Jain <shainkeyjain30@gmail.com>
2019-12-05 00:36:09 +0530

    code refactoring of callback

--- callback/callback.js
+++ callback/callback.js
@@ -30,3 +30,42 @@ function getStudents(){
 let newStudent = {name:"Shainkey aditya jain", subject:"Python"}
 enrollStudent(newStudent, getStudents);
 // getStudents();
+
+
+//  Above example by using promise
+
+function enrollStudent(student){
+    return new Promise((resolve, reject)=>{
+        setTimeout(function() {
+            students.push(student);
+            console.log("Student has been enrolled");
+            const error = false;
+            if(!error){
+                resolve();
+            }else {
+                reject();
+            }
+        },5000);
+    })
+
+}
+
+function getStudents(){
+    setTimeout(function() {
+        let str = "";
+        students.forEach(function(student) {
+            str += `<li> ${student.name}</li>`
+        })
+        document.getElementById('students').innerHTML = str;
+        console.log('Student have been fetched');
+    },4000);
+
+}
+let newStudent = {name:"Shainkey aditya jain", subject:"Python"}
+enrollStudent(newStudent).then(getStudents).catch((error)=>{
+    console.log("Promise has been rejected");
+})
+/* important points
+1- function inside then is run as promise resolve
+2- function inside catch is run as promise reject
+*/
